d28d674153e9d130d0a186dbc81e8b469fed0da5
Bisrat Girma <bisrat@eaglelionsystems.com>
2023-11-11 01:05:00 +0300

    inter font weight's added

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,18 +1,30 @@
 import Image from "next/image";
-import nextFont from "next/font";
+import { Inter } from "next/font/google";
 
 import HeroImage from "../../../public/xy college hero.svg";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "800", "900"],
+});
 
 function Hero() {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-6xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+        <div
+          className={`mr-auto place-self-center lg:col-span-7 ${inter.className}`}
+        >
+          <h1 className="max-w-2xl mb-4 text-6xl font-black tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
             Harmony in Learning
           </h1>
-          <p className="max-w-2xl mb-6 font-extrabold font- text-primary-500 lg:mb-8 text-6xl">
+          <p className="max-w-2xl mb-3 font-extrabold text-primary-500 text-6xl">
             Uniting Curiosity and Knowledge
+          </p>
+          <p className="max-w-2xl mb-9 text-[#6B7280] text-xl font-normal">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
           </p>
           <a
             href="#"
